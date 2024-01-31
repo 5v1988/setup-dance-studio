b500b2cc5403f84399a1e6c132d35da87665ab7e
Veera <nmani0303@gmail.com>
2024-01-31 16:32:22 +0800

    fix readme

--- bin/install.mjs
+++ bin/install.mjs
@@ -3,6 +3,7 @@ import { promisify } from "util";
 import cp from "child_process";
 import path from "path";
 import fs from "fs";
+import chalk from 'chalk';
 // cli spinners
 import ora from "ora";
 
@@ -32,12 +33,12 @@ else {
 }
 
 try {
-  const gitSpinner = ora("Downloading files...").start();
+  const gitSpinner = ora("Downloading project setup files...").start();
   // clone the repo into the project folder -> creates the new boilerplate
   await exec(`git clone --depth 1 ${gitRepo} ${projectPath} --quiet`);
   gitSpinner.succeed();
 
-  const cleanSpinner = ora("Removing unwanted files").start();
+  const cleanSpinner = ora("Removing unwanted files...").start();
   // remove my git history
   const rmGit = rm(path.join(projectPath, ".git"), { recursive: true, force: true });
   // remove the installation file
@@ -53,18 +54,19 @@ try {
   await exec("npm install");
   npmSpinner.succeed();
 
+  const npmUpdateSpinner = ora("Updating dependencies...").start();
   await exec("npm update");
-  npmSpinner.succeed();
+  npmUpdateSpinner.succeed();
 
+  const npmPlaywrigthSpinner = ora("Setting up Playwright dependencies...").start();
   await exec("npx playwright install");
   npmSpinner.succeed();
-
-  console.log('The project setup is completed!');
-  console.log('Next steps: 1. Run example tests using the below command:');
-  console.log(`                cd ${projectName} && npm run test`);
-  console.log('            2. Build your own tests, by following the steps mentioned here:');
-  console.log('                https://github.com/5v1988/demo-dancing-yaml');
-
+  console.log(chalk.green('The project setup is completed!'));
+  console.log(chalk.green('Next steps: 1. Run example tests using the below command:'));
+  console.log(chalk.yellow('                cd ', chalk.bold('%s'), '  && npm run test'),
+    projectName);
+  console.log(chalk.blue('            2. Build your own tests, by following the steps from here:'));
+  console.log(chalk.yellow.bold('                https://github.com/5v1988/demo-dancing-yaml')
 } catch (error) {
   // clean up in case of error, so the user does not have to do it manually
   fs.rmSync(projectPath, { recursive: true, force: true });
